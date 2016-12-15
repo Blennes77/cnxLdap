b846efd22fbdb2691aca9c63434d2fa570e7efb1
garnons <blennes77@gmail.com>
2016-12-15 11:48:39 +0100

    ok

--- src/main/java/com/cgihosting/constantes/Constantes.java
+++ src/main/java/com/cgihosting/constantes/Constantes.java
@@ -7,4 +7,5 @@ public class Constantes {
     public static final int ROLE_ADMIN = 2;
     public static final int ROLE_EXPLOITANT = 3;
     public static final int ROLE_USER = 1;
+    public static final int ROLE_DP = 4;
 }
